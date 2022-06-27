2b4dbce6c0db5e77844e27eff725fc933390f88b
Sujit <sujit.patil72@hotmail.com>
2022-06-27 10:40:32 +0530

    30. Monsters Rolodex - States and Shallow Merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Yihua"
+      name: { firstName: "Yihua", lastName: "Zhang" },
+      company: "ZTM"
     }
   }
 
@@ -17,10 +18,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name}
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
           </p>
           <button onClick={() => {
-            this.setState({ name: "Andrei" })
+            this.setState({ name: { firstName: "Andrei", lastName: "Neaogie" } })
           }}>Change Name</button>
         </header>
       </div>
