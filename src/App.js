3314da03bbf210df71b5fd19afe393478a84b649
Sujit <sujit.patil72@hotmail.com>
2022-06-27 10:08:54 +0530

    28. Monsters Rolodex - Component State

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+class App extends Component {
 
-function App() {
-  console.log("test");
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Andrei"
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name}
+          </p>
+          <button>Change Name</button>
+        </header>
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
