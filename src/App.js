aaac711b6202d9c6e2fe4fbc5c777ddb9af2725b
sujitpatil72 <sujit.patil72@hotmail.com>
2022-06-28 11:19:39 +0530

    46. Monsters Rolodex - Component Props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 import CardList from "./components/card-list/card-list.component";
@@ -10,20 +9,15 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
-    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => console.log(this.state)
-        )
+        this.setState(() => {
+          return { monsters: users };
+        })
       );
   }
 
@@ -36,8 +30,6 @@ class App extends Component {
   };
 
   render() {
-    console.log("render");
-
     const { monsters, searchField } = this.state;
     const { onsearchChange } = this;
 
@@ -53,14 +45,7 @@ class App extends Component {
           placeholder="search monsters"
           onChange={onsearchChange}
         />
-        {/* {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })} */}
-        <CardList />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,21 @@
 import { Component } from "react";
 
 class CardList extends Component {
-
-    render(){
-        return <div>Hello Cardlist here</div>
-    }
+  render() {
+    console.log("render");
+    const { monsters } = this.props;
+    return (
+      <div>
+        {monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-export default CardList
+export default CardList;
