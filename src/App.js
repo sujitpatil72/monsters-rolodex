ee7cf735c1219e3a2aa5b95bcd396c8977a43b1b
sujitpatil72 <sujit.patil72@hotmail.com>
2022-06-28 09:58:58 +0530

    38. Monsters Rolodex - Renders & Re-renders in React

--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+
+    console.log("1");
   }
 
   componentDidMount() {
+    console.log("3");
+
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -24,6 +28,8 @@ class App extends Component {
   }
 
   render() {
+    console.log("2");
+
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
