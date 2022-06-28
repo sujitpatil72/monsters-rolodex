75263066cfea28c5bf343e94054b57ae1f53bcc2
sujitpatil72 <sujit.patil72@hotmail.com>
2022-06-28 09:30:07 +0530

    36. Monsters Rolodex - Lifecycle Method: componentDidMount

--- src/App.js
+++ src/App.js
@@ -6,31 +6,27 @@ class App extends Component {
     super();
 
     this.state = {
-      monster: [
-        {
-          name: "Linda",
-          id: "12e123",
-        },
-        {
-          name: "Frank",
-          id: "12e124",
-        },
-        {
-          name: "Jacky",
-          id: "12e125",
-        },
-        {
-          name: "Andrei",
-          id: "12e126",
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => console.log(this.state)
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monster.map((monster) => {
+        {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
