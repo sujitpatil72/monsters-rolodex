30780838623120fd78fb97d3c1338a60b3d1f88e
sujitpatil72 <sujit.patil72@hotmail.com>
2022-06-28 08:51:54 +0530

    Render monsters without loop

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 class App extends Component {
-
   constructor() {
     super();
 
     this.state = {
-      name: { firstName: "Yihua", lastName: "Zhang" },
-      company: "ZTM"
-    }
+      monster1: {
+        name: "Linda",
+      },
+      monster2: {
+        name: "Frank",
+      },
+      monster3: {
+        name: "Jacky",
+      },
+    };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState(
-              () => {
-                return {
-                  name: { firstName: "Andrei", lastName: "Neaogie" }
-                }
-              }, () => console.log(this.state))
-
-          }
-          }>Change Name</button>
-        </header>
+        <h1>{this.state.monster1.name}</h1>
       </div>
     );
   }
-
-
 }
 
 export default App;
