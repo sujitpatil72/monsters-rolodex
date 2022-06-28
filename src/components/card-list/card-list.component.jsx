eb4990d5f7691356ada7d1c108f12d8a2c759869
sujitpatil72 <sujit.patil72@hotmail.com>
2022-06-28 11:25:21 +0530

    47. Monsters Rolodex - Rendering and Re-rendering part 2

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,7 +2,8 @@ import { Component } from "react";
 
 class CardList extends Component {
   render() {
-    console.log("render");
+    console.log(this.props.monsters);
+    console.log("render from cardlist");
     const { monsters } = this.props;
     return (
       <div>
