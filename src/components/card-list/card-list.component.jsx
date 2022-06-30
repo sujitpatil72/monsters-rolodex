586f2eac47ceb459ab4bdd4cc05581934a8ca9a1
sujitpatil72 <sujit.patil72@hotmail.com>
2022-06-30 09:32:06 +0530

    52. Monsters Rolodex - Finishing Touches

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,8 @@
 import { Component } from "react";
+import Card from "../card/card.component";
 
 import "./card-list.styles.css";
+
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
@@ -8,17 +10,7 @@ class CardList extends Component {
     return (
       <div className="card-list">
         {monsters.map((monster) => {
-          const { name, id, email } = monster;
-          return (
-            <div className="card-container" key={monster.id}>
-              <img
-                alt={`monster ${name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} key={monster.id} />;
         })}
       </div>
     );
