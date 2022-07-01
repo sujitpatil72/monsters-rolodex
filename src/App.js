27ec340e9ad718b4ae4d0c74d9d388cda9285abe
sujitpatil72 <sujit.patil72@hotmail.com>
2022-07-01 16:45:42 +0530

    60. Monsters Rolodex - Hooks: useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
 const App = () => {
-  console.log("render");
   const [searchField, setSearchField] = useState("a");
-  console.log("🚀 ~ file: App.js ~ line 9 ~ App ~ searchField", searchField);
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+
+    console.log("effect is firing");
+  }, [searchField, monsters]);
 
   const onsearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
@@ -23,6 +39,7 @@ const App = () => {
         onChangeHandler={onsearchChange}
         placeholder="search monsters"
       />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 };
